37167a0a49e7d3f0ebb8d200819128d369030492
Shiva-Kasula <120006180+Shiva-Kasula@users.noreply.github.com>
2022-12-07 15:34:31 +0530

    Add files via upload

---
+++ ShowDateTime.java
@@ -0,0 +1,11 @@
+package com.nt.git;
+
+import java.time.LocalDateTime;
+
+public class ShowDateTime {
+	public static void main(String[] args) {
+		LocalDateTime ldt=LocalDateTime.now();
+        System.out.println("TIME AND DATE  :  "+ldt);
+	}
+
+}
